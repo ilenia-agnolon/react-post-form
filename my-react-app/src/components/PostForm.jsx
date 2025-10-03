fba7600ef09da53c4b81183c40922f04339b0b6f
ilemia-agnolon <ilenia.agnolon.96@gmail.com>
2025-10-03 16:00:38 +0200

    - inserito form pre-impostato tramite bootstrap
    - verificato endpoint a cui effettuare la chiamata POST tramite postman

--- my-react-app/src/components/PostForm.jsx
+++ my-react-app/src/components/PostForm.jsx
@@ -1,3 +1,63 @@
 import { useState } from "react";
 
+// API https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts
+
+const PostForm = () => {
+  return (
+    <>
+      <div className="container">
+        <form>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              autore del post
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+            <div id="emailHelp" className="form-text">
+              We'll never share your email with anyone else.
+            </div>
+          </div>
+          <div class="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              titolo del post
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleFormControlTextarea1" className="form-label">
+              testo del post
+            </label>
+            <textarea
+              className="form-control"
+              id="exampleFormControlTextarea1"
+              rows="3"
+            ></textarea>
+          </div>
+          <div class="mb-3 form-check">
+            <input
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" for="exampleCheck1">
+              pubblico
+            </label>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Invia
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
 export default PostForm;
